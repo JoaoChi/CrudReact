9aad2655f43a42933b7f4be9e8726fd797e4bd50
Joao Gabriel Chiarello <joao.chiarello@unochapeco.edu.br>
2024-11-14 17:10:40 -0300

    adicionado botao para editar e excluir usuarios

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,16 +1,37 @@
 import React from "react";
 import { View, FlatList } from "react-native";
 import users from "../data/users";
-import { ListItem, Avatar } from "react-native-elements";
+import { ListItem, Avatar, Button, Icon } from "react-native-elements";
 
 export default props => {
+
+    function getActions(user) {
+        return (
+            <>
+                <Button
+                    onPress={() => props.navigation.navigate('UserForm', user)}
+                    type="clear"
+                    icon={<Icon name="edit" size={25} color="orange" />}
+                />
+                <Button
+                    onPress={() => console.log("Deleting user", user)}
+                    type="clear"
+                    icon={<Icon name="delete" size={25} color="red" />}
+                />
+            </>
+        );
+    }
+
     function getUserItem({ item: user }) {
         return (
-            <ListItem key={user.id} bottomDivider onPress={() => props.navigation.navigate('UserForm')}>
+            <ListItem key={user.id.toString()} bottomDivider>
                 <Avatar source={{ uri: user.avatarUrl }} />
                 <ListItem.Content>
                     <ListItem.Title>{user.name}</ListItem.Title>
                     <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+                </ListItem.Content>
+                <ListItem.Content right>
+                    {getActions(user)}
                 </ListItem.Content>
             </ListItem>
         );
