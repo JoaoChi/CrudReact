747bee561edde0a2af729ef795d67d037a7a949d
Joao Gabriel Chiarello <joao.chiarello@unochapeco.edu.br>
2024-11-14 16:28:34 -0300

    Criado classe de parametro e navegação entre as telas.

---
+++ src/data/users.js
@@ -0,0 +1,33 @@
+export default [
+    {
+      id: 1,
+      name: "Alice Johnson",
+      email: "alice.johnson@example.com",
+      avatarUrl: "https://cdn.pixabay.com/photo/2016/03/31/20/31/amazed-1295833_1280.png",
+    },
+    {
+      id: 2,
+      name: "Bob Smith",
+      email: "bob.smith@example.com",
+      avatarUrl: "https://cdn.pixabay.com/photo/2016/03/31/20/27/avatar-1295773_1280.png",
+    },
+    {
+      id: 3,
+      name: "Charlie Brown Juniors",
+      email: "charlie.brown@example.com",
+      avatarUrl: "https://cdn.pixabay.com/photo/2016/03/31/20/31/avatar-1295831_1280.png",
+    },
+    {
+      id: 4,
+      name: "Daisy Evans",
+      email: "daisy.evans@example.com",
+      avatarUrl: "https://cdn.pixabay.com/photo/2016/03/31/20/11/avatar-1295575_1280.png",
+    },
+    {
+      id: 5,
+      name: "Ethan Williams",
+      email: "ethan.williams@example.com",
+      avatarUrl: "https://cdn.pixabay.com/photo/2016/03/31/21/40/angry-1296580_1280.png",
+    },
+  ];
+  
--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 
 export default props => {
     return (
-        <Text>
-            UserList
-        </Text>
+        <View>
+            <FlatList
+                data={ }
+            />
+        </View>
     )
 }
