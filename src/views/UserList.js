be9d9c76a5e256aac81e0f6653ba97a6f9e1e10b
Joao Gabriel Chiarello <joao.chiarello@unochapeco.edu.br>
2024-11-14 16:55:22 -0300

    Mostrando usuarios cadastrados na lista de usuarios (primeira página)

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,12 +1,28 @@
 import React from "react";
-import { View, Text, FlatList } from "react-native";
+import { View, FlatList } from "react-native";
+import users from "../data/users";
+import { ListItem, Avatar } from "react-native-elements";
 
 export default props => {
+    function getUserItem({ item: user }) {
+        return (
+            <ListItem key={user.id} bottomDivider onPress={() => props.navigation.navigate('UserForm')}>
+                <Avatar source={{ uri: user.avatarUrl }} />
+                <ListItem.Content>
+                    <ListItem.Title>{user.name}</ListItem.Title>
+                    <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+                </ListItem.Content>
+            </ListItem>
+        );
+    }
+
     return (
         <View>
             <FlatList
-                data={ }
+                keyExtractor={user => user.id.toString()}
+                data={users}
+                renderItem={getUserItem}
             />
         </View>
-    )
-}
+    );
+};
